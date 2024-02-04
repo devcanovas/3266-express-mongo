2c2904abda22f0c30e160fc683673009fbd00e00
Matheus Canovas <matheuscanovas9@gmail.com>
2024-02-04 16:58:03 -0300

    feat: Implementing the endpoint /books with GET method

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ import express from "express";
 
 const app = express();
 
+const books = [
+  {
+    id: 1,
+    title: "The Lord of Rings",
+  },
+  {
+    id: 2,
+    title: "Harry Potter",
+  },
+];
+
 app.get("/", (req, res) => {
   res.status(200).send("Node.js course");
+});
+
+app.get("/books", (req, res) => {
+  res.status(200).json(books);
 });
 
 export default app;
